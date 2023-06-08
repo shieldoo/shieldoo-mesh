d9c3f2f4eb7969f5533c67bd4c69e339fff15195
Jakub Klapka <jakub-klapka@users.noreply.github.com>
2023-06-08 10:58:16 +0200

    fix: Independent Docker build and service startup (#18)
    * Add docker multistage build
    
    * Add simple docu for docker run
    
    * Trap not needed when using exec
    
    * Fix sonarcube findings

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo '
+       .__    .__       .__       .___             
+  _____|  |__ |__| ____ |  |    __| _/____   ____  
+ /  ___/  |  \|  |/ __ \|  |   / __ |/  _ \ /  _ \ 
+ \___ \|   Y  \  \  ___/|  |__/ /_/ (  <_> |  <_> )
+/____  >___|  /__|\___  >____/\____ |\____/ \____/ 
+     \/     \/        \/           \/             '
+echo ''
+
+mkdir -p /dev/net
+if [ ! -c /dev/net/tun ]; then
+    mknod /dev/net/tun c 10 200
+fi
+
+echo ''
+echo 'starting shieldoo ..'
+
+exec /app/shieldoo-mesh-srv -run
