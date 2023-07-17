6aabab3b88ede3fd759352f15d62fe239d2f430b
valda <valda@cloudfield.cz>
2023-07-17 07:44:09 +0200

    fix: yum support

--- osupdate-script-linux.sh
+++ osupdate-script-linux.sh
@@ -8,13 +8,15 @@
 # dpkg --configure -a
 #
 # dnf upgrade
+# 
+# yum update
 
 # Return values:
 # 0	If no error occured
 # 1	apt-get and dnf not found (by the "command" shell builtin) 
 # 2	Wrong usage
-# 3     A command that does not upgrade packages failed (apt-get update, apt-get dist-upgrade -s, dnf check-update)
-# 4	A command that upgrades packages failed (apt-get dist-upgrade, apt-get install, dnf upgrade)
+# 3     A command that does not upgrade packages failed (apt-get update, apt-get dist-upgrade -s, dnf check-update, yum check-update)
+# 4	A command that upgrades packages failed (apt-get dist-upgrade, apt-get install, dnf upgrade, yum update)
 # 5	apt-check utility returns different number of security updates,
 #	this probalby means that output format of 'apt-get -s -qq dist-upgrade' changed, which has broken this script
 
@@ -176,17 +178,14 @@ dnf_list_other_updates() {
 
 # Function to perform security/all update depending on parameter
 dnf_perform_update() {
-	#perform non-interactive upgrade, if --security parameter is provided,
-	#perform only security patches
+	#perform non-interactive upgrade
+	#if --security parameter is provided, perform only security patches
 	#quoting of the variable containing options is ommited intentionally to enable word splitting	
-  	if ! dnf $NONINTERACTIVE_OPTS $1 upgrade;
-	then
-		#if dnf returns 1 it means an error occurred but dnf was able to solve it
-		#in that case continue the script
-		[[ "$?" -eq 1 ]] && return 0
-		error "dnf $NONINTERACTIVE_OPTS $1 upgrade" "$?" >&2
-		exit 4	
-	fi
+	output="$(dnf $NONINTERACTIVE_OPTS $1 upgrade)"
+	exit_code="$?"
+	
+	#if dnf returns 1 it means an error occurred but dnf was able to solve it
+	[[ "$exit_code" -ne 0 && "$exit_code" -ne 1 ]] && { echo "$output" >&2; error "dnf $NONINTERACTIVE_OPTS $1 upgrade" "$exit_code" >&2; exit 4; }
 }
 
 # Function to perform security updates
@@ -207,6 +206,75 @@ dnf_recover(){
 # ======================================================================================
 # ======================================================================================
 
+# ======================================================================================
+# =========================FUNCTIONS USING YUM (FOR CENTOS 7)===========================
+
+# Function to display security/all updates based on parameter
+yum_find_updates() {
+	#set options based on positional parameter
+	#-q is for omitting unnecessary information in the output
+	#--security is for filtering security packages
+	opt="-q"; [[ "$1" == "security" ]] && opt="-q --security"		
+
+	#if check-update fails (exit code 100 means no updates have been found),
+	#echo the error messages and exit the script
+	#else parse the output and continue the script
+	output="$(yum $opt check-update)"
+	exit_code="$?"
+	[[ "$exit_code" -ne 0 && "$exit_code" -ne 100 ]] && { echo "$output" >&2; error "yum $opt check-update" "$exit_code" >&2; exit 3; }
+	printf '%s\n' "$output" | cut -d' ' -f1 | grep -v '^$' | sed 's/\.[^\.]*$//'
+}
+
+# Function to display security updates
+yum_list_security_updates(){
+	yum_find_updates "security"
+}
+
+# Function to display non-security updates
+yum_list_other_updates() {
+	#create temporary file
+	tmpfile=$(mktemp) || { error "mktemp" "$?" >&2; exit 3; }
+	
+	#store the sorted list of all updates in the tmp file
+	#sort is necessary for comm command to work
+	yum_find_updates "all" | sort  > "$tmpfile"
+	
+	#use comm to display the lines that appear only in the tmpfile,
+	#thus are not security updates
+	yum_find_updates "security" | sort | comm -23 "$tmpfile" -
+	
+	#clean-up
+	rm "$tmpfile"
+}
+
+# Function to perform security/all update depending on parameter
+yum_perform_update() {
+	#perform non-interactive upgrade
+	#if --security parameter is provided, perform only security patches
+	#quoting of the variable containing options is ommited intentionally to enable word splitting	
+	output="$(yum $NONINTERACTIVE_OPTS update $1)"
+	exit_code="$?"
+	[[ "$exit_code" -ne 0 ]] && { echo "$output"; error "yum $NONINTERACTIVE_OPTS update $1" "$exit_code" >&2; exit 4; }
+}
+
+# Function to perform security updates
+yum_perform_security_updates(){
+	yum_perform_update "--security"
+}
+
+# Function to perform all updates
+yum_perform_all_updates(){
+	yum_perform_update ""
+}
+
+# Function to recover from upgrade interrupt
+yum_recover(){
+	echo "No automated fixes are available, manual repair is needed."
+}
+
+# ======================================================================================
+# ======================================================================================
+
 # Check what package management software is present on the system and set corresponding variables.
 if command -v apt-get &> /dev/null; then
     	SOFTWARE="apt"
@@ -216,8 +284,11 @@ if command -v apt-get &> /dev/null; then
 elif command -v dnf &> /dev/null; then
 	SOFTWARE="dnf"
 	NONINTERACTIVE_OPTS='-y'
+elif command -v yum &> /dev/null; then
+	SOFTWARE="yum"
+	NONINTERACTIVE_OPTS='-y'
 else	
-	echo 'apt-get and dnf not found (by the "command" shell builtin)' 
+	echo 'None of the supported package managers (APT, DNF, YUM) found (by the "command" shell builtin)' 
 	exit 1
 fi	
 
